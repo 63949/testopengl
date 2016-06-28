cbf3363ae23f5aa8e7577d9e5a695d93d9fe85da
罗培中 <lpz2@163.com>
2016-06-28 11:11:51 +0800

    opengl RGB颜色

--- main.cpp
+++ main.cpp
@@ -25,48 +25,9 @@ void init()
 }
 void display()
 {
-      static GLubyte Mask[128];
-      FILE *fp;
-      fp = fopen("/home/lpz/testopengl/test.bmp","rb");
-      if(!fp)
-      {
-          printf("file not exit.\n");
-          exit(0);
-      }
-      if(fseek(fp,-(int)sizeof(Mask),SEEK_END))
-          exit(0);
-      if(!fread(Mask,sizeof(Mask),1,fp))
-          exit(0);
-      fclose(fp);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(0.0f,1.0f,1.0f);
+    glRectf(-0.5f,-0.5f,0.5f,0.5f);
+    glFlush();
 
-      glClear(GL_COLOR_BUFFER_BIT);
-      glEnable(GL_POLYGON_STIPPLE);
-      glPolygonStipple(Mask);
-      glRectf(-0.5f,-1.5f,0.0f,0.0f);
-      glDisable(GL_POLYGON_STIPPLE);
-      glRectf(0.0f,0.0f,0.5f,0.5f);
-      glFlush();
-      //glColor3f(1.0, 0, 0);
-     /* glPolygonMode(GL_FRONT,GL_FILL);
-      glPolygonMode(GL_BACK,GL_LINE);
-      glFrontFace(GL_CCW);
-*/
-      //glBegin(GL_LINES);
-/*      glBegin(GL_POLYGON);
-      glVertex2f(-0.5f,-0.5f);
-      glVertex2f(0.0f,-0.5f);
-      glVertex2f(0.0f,0.0f);
-      glVertex2f(-0.5f,0.0f);
- */     /*glVertex2f(0.0f,0.0f);
-      glVertex2f(10.5f,0.5f);
-*/
-  //    glEnd();
-//      glutWireTeapot(3);
-   /*   glBegin(GL_POLYGON);
-      glVertex2f(0.0f,0.0f);
-      glVertex2f(0.0f,0.5f);
-      glVertex2f(0.5f,0.5f);
-      glVertex2f(0.5f,0.0f);
-      glEnd();
-      glFlush();*/
 }
