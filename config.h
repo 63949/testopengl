f9156f1f063080742aef6c71921cf2d59142e82c
罗培中 <lpz2@163.com>
2016-06-28 10:20:55 +0800

    opengl环境配置

---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "testopengl"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "lpz2@163.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "testopengl"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "testopengl 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "testopengl"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
